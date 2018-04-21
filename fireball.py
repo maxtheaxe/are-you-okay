517dcca1ad458592ef63fcd7a8a0c02f39cf86fa
maxtheaxe <max.perrello@gmail.com>
2018-04-21 13:18:22 -0400

    Update fireball.py

--- fireball.py
+++ fireball.py
@@ -1,4 +1,4 @@
 from firebase import firebase
-firebase = firebase.FirebaseApplication('https://testing-4f663.firebaseio.com/', None)
+firebase = firebase.FirebaseApplication('https://testing-97c52.firebaseio.com/users', None)
 result = firebase.get('/users', '1')
 print result
