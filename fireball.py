222e007581ddd845756a1042b7924e2b1f1f80b3
maxtheaxe <max.perrello@gmail.com>
2018-04-21 13:24:44 -0400

    Update fireball.py

--- fireball.py
+++ fireball.py
@@ -1,4 +1,7 @@
 from firebase import firebase
 firebase = firebase.FirebaseApplication('https://testing-97c52.firebaseio.com/users', None)
-result = firebase.get('/users', '1')
+new_user = 'Ozgur Vatansever'
+
+result = firebase.post('/users', new_user, {'print': 'pretty'}, {'X_FANCY_HEADER': 'VERY FANCY'})
 print result
+{u'name': u'-Io26123nDHkfybDIGl7'}
