40712b485927335f9d912a5a918355901ab69d06
maxtheaxe <max.perrello@gmail.com>
2018-04-21 14:49:09 -0400

    Update fireball.py

--- fireball.py
+++ fireball.py
@@ -2,5 +2,5 @@ from firebase import firebase
 firebase = firebase.FirebaseApplication('https://friendlychat-d891b.firebaseio.com/', None)
 new_user = 'kgigklg'
 
-firebase.post('/beneatass', new_user, 'random')
+firebase.put('/beneatass/xloc', new_user)
 
