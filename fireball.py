063bb051900de41447e937a14aaa22e01da906ac
maxtheaxe <max.perrello@gmail.com>
2018-04-21 14:08:29 -0400

    Update fireball.py

--- fireball.py
+++ fireball.py
@@ -1,8 +1,14 @@
 from firebase import firebase
+import json
+
 firebase = firebase.FirebaseApplication('https://friendlychat-d891b.firebaseio.com/', None)
 new_user = '19248.23405'
 
+xloc = 'xloc'
 
+data = {'xloc': new_user'}
+        
+set = json.dumps(data)
 
-result = firebase.post('/test/xloc', new_user)
+result = firebase.post('/test, sent)
 print result
