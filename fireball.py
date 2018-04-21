bae57938de4727b38a3ad321fab3cf07f767241a
maxtheaxe <max.perrello@gmail.com>
2018-04-21 13:09:18 -0400

    Update fireball.py

--- fireball.py
+++ fireball.py
@@ -1,4 +1,4 @@
 from firebase import firebase
-firebase = firebase.FirebaseApplication('https://cbb-hackathon.firebaseio.com/', None)
+firebase = firebase.FirebaseApplication('https://testing-4f663.firebaseio.com/', None)
 result = firebase.get('/users', None)
 print result
