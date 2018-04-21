84d1f0d39064f731cb71a7982e9572a5ff0b2ece
maxtheaxe <max.perrello@gmail.com>
2018-04-21 14:22:36 -0400

    Update fireball.py

--- fireball.py
+++ fireball.py
@@ -1,5 +1,5 @@
 from firebase import firebase
-firebase = firebase.FirebaseApplication('https://your_storage.firebaseio.com', None)
+firebase = firebase.FirebaseApplication('https://friendlychat-d891b.firebaseio.com/', None)
 new_user = 'Ozgur Vatansever'
 
 result = firebase.post('/beneatass', new_user)
