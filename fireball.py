7831bfbd7bb7981c96737372b6bf2fe28ea5ac07
maxtheaxe <max.perrello@gmail.com>
2018-04-21 13:02:47 -0400

    Update fireball.py

--- fireball.py
+++ fireball.py
@@ -1,36 +1,4 @@
-import RPi.GPIO as GPIO
-from time import sleep
-import datetime
 from firebase import firebase
-
-
-import urllib2, urllib, httplib
-import json
-import os 
-from functools import partial
-
-
-
-
-
-# Try to grab a sensor reading.  Use the read_retry method which will retry up
-# to 15 times to get a sensor reading (waiting 2 seconds between each retry).
-
-
-
-firebase = firebase.FirebaseApplication('https://cbb-hackathon.firebaseapp.com/', None)
-#firebase.put("/dht", "/temp", "0.00")
-#firebase.put("/dht", "/humidity", "0.00")
-
-def update_firebase():
-
-	
-	firebase.post('hello')
-	
-
-while True:
-		update_firebase()
-		
-        #sleepTime = int(sleepTime)
-		sleep(5)
-	
+firebase = firebase.FirebaseApplication('https://cbb-hackathon.firebaseio.com/', None)
+result = firebase.get('/users', None)
+print result
