283284848eaea5d9685927099989a64192bebdad
maxtheaxe <max.perrello@gmail.com>
2018-04-21 13:43:15 -0400

    Update fireball.py

--- fireball.py
+++ fireball.py
@@ -2,5 +2,7 @@ from firebase import firebase
 firebase = firebase.FirebaseApplication('https://cbb-hackathon.firebaseio.com/', None)
 new_user = '19248.23405'
 
-result = firebase.post('/users', new_user)
+
+
+result = firebase.post('/test/xloc', new_user)
 print result
