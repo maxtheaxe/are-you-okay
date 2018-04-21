1b9e15b3ded2378e9aeb27cd1b7ceef01926bf53
maxtheaxe <max.perrello@gmail.com>
2018-04-21 13:32:21 -0400

    Update fireball.py

--- fireball.py
+++ fireball.py
@@ -1,6 +1,6 @@
 from firebase import firebase
-firebase = firebase.FirebaseApplication('https://testing-97c52.firebaseio.com/users', None)
-new_user = 'Ozgur Vatansever'
+firebase = firebase.FirebaseApplication('https://cbb-hackathon.firebaseio.com/', None)
+new_user = '19248.23405'
 
 result = firebase.post('/users', new_user)
 print result
