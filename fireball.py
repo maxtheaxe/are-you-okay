5d606f2d099f86f124378dc838703e91d7525160
maxtheaxe <max.perrello@gmail.com>
2018-04-21 14:21:45 -0400

    Update fireball.py

--- fireball.py
+++ fireball.py
@@ -2,5 +2,5 @@ from firebase import firebase
 firebase = firebase.FirebaseApplication('https://your_storage.firebaseio.com', None)
 new_user = 'Ozgur Vatansever'
 
-result = firebase.post('/messages', new_user)
+result = firebase.post('/beneatass', new_user)
 print result
