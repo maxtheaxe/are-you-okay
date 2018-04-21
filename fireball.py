df0525466a5fbb4e1cd2514cb588ae5bb6c24bbc
maxtheaxe <max.perrello@gmail.com>
2018-04-21 13:51:37 -0400

    Update fireball.py

--- fireball.py
+++ fireball.py
@@ -1,5 +1,5 @@
 from firebase import firebase
-firebase = firebase.FirebaseApplication('https://friendlychat-8cd27.firebaseapp.com/', None)
+firebase = firebase.FirebaseApplication('https://friendlychat-d891b.firebaseio.com/', None)
 new_user = '19248.23405'
 
 
